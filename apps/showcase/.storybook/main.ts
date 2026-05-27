46c6acc9d1e0d171c624b04c1034f251242cb9d3
Prabakar Karuppasamy <prabakarkaruppasamy@gmail.com>
2026-05-27 11:43:12 +0530

    feat: configure Storybook v10 with Angular 21 and Tailwind v4

--- apps/showcase/.storybook/main.ts
+++ apps/showcase/.storybook/main.ts
@@ -1,16 +1,20 @@
+import { fileURLToPath } from "node:url";
+import { dirname } from "node:path";
 import type { StorybookConfig } from '@storybook/angular';
 
 const config: StorybookConfig = {
   stories: ['../src/app/**/*.@(mdx|stories.@(js|jsx|ts|tsx))'],
   addons: [],
   framework: {
-    name: '@storybook/angular',
-    options: {},
+    name: getAbsolutePath("@storybook/angular"),
+    options: {
+      project: 'showcase',
+    },
   },
 };
 
 export default config;
 
-// To customize your webpack configuration you can use the webpackFinal field.
-// Check https://storybook.js.org/docs/react/builders/webpack#extending-storybooks-webpack-config
-// and https://nx.dev/recipes/storybook/custom-builder-configs
+function getAbsolutePath(value: string): any {
+  return dirname(fileURLToPath(import.meta.resolve(`${value}/package.json`)));
+}
